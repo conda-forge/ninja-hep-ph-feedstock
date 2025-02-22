c29b57af9f1143423ab22e904a35208e21917b61
Matthew Feickert <matthew.feickert@cern.ch>
2025-02-21 19:43:22 -0700

    ENH: Make quadninja optional and add macOS, aarch64 Linux builds (#7)
    * Enable quadninja optionally based on if libquadmath is available.
    * Add osx and linux-aarch64 builds.
       - Add gawk, grep, and sed as 'build' and 'test' requirements to use the correct versions.
       - Don't build against looptools-static on macOS arm64, as there isn't a build for osx-arm64 yet.
       - Add patch from https://github.com/peraro/ninja/pull/6 to remove VERSION file.
    * Add ABI migration rebuilds for ninja-hep-ph v1.1.0 on branch 1.x.
    * Bump build number.

--- recipe/build_shared.sh
+++ recipe/build_shared.sh
@@ -1,5 +1,17 @@
 #!/bin/bash
 
+set -xe
+
+export DISABLE_QUADMATH=false
+
+# libquadmath not supported on macOS or aarch64
+if [[ "$(uname)" == "Darwin" ]]; then
+    export DISABLE_QUADMATH=true
+fi
+if [[ "${target_platform}" == linux-aarch64 ]]; then
+    export DISABLE_QUADMATH=true
+fi
+
 # c.f. https://conda-forge.org/docs/maintainer/knowledge_base/#cross-compilation-examples
 # Get an updated config.sub and config.guess
 cp $BUILD_PREFIX/share/gnuconfig/config.* .
@@ -10,16 +22,42 @@ autoreconf --install
 
 # Remove static library to comply with CFEP-18
 # https://github.com/conda-forge/cfep/blob/main/cfep-18.md
-./configure \
-    --prefix=$PREFIX \
-    --enable-shared=yes \
-    --enable-static=no \
-    --enable-higher_rank \
-    --enable-quadninja \
-    --with-avholo="$FFLAGS -lavh_olo" \
-    --with-looptools="$FLDFLAGS -looptools -lgfortran -lquadmath" \
-    FCINCLUDE="${FCINCLUDE} -I$PREFIX/include/oneloop" \
-    CPPFLAGS="${CPPFLAGS} -DNINJA_NO_EXCEPTIONS"
+if [[ "${DISABLE_QUADMATH}" == true ]]; then
+    echo -e "\n# libquadmath not supported on target platform ${target_platform} so disabling quadninja."
+    if [[ "$(uname)" == "Darwin" ]]; then
+        ./configure \
+            --prefix=$PREFIX \
+            --enable-shared=yes \
+            --enable-static=no \
+            --enable-higher_rank \
+            --disable-quadninja \
+            --with-avholo="$FFLAGS -lavh_olo" \
+            FCINCLUDE="${FCINCLUDE} -I$PREFIX/include/oneloop" \
+            CPPFLAGS="${CPPFLAGS} -DNINJA_NO_EXCEPTIONS"
+    else
+        ./configure \
+            --prefix=$PREFIX \
+            --enable-shared=yes \
+            --enable-static=no \
+            --enable-higher_rank \
+            --disable-quadninja \
+            --with-avholo="$FFLAGS -lavh_olo" \
+            --with-looptools="$FLDFLAGS -looptools -lgfortran" \
+            FCINCLUDE="${FCINCLUDE} -I$PREFIX/include/oneloop" \
+            CPPFLAGS="${CPPFLAGS} -DNINJA_NO_EXCEPTIONS"
+    fi
+else
+    ./configure \
+        --prefix=$PREFIX \
+        --enable-shared=yes \
+        --enable-static=no \
+        --enable-higher_rank \
+        --enable-quadninja \
+        --with-avholo="$FFLAGS -lavh_olo" \
+        --with-looptools="$FLDFLAGS -looptools -lgfortran -lquadmath" \
+        FCINCLUDE="${FCINCLUDE} -I$PREFIX/include/oneloop" \
+        CPPFLAGS="${CPPFLAGS} -DNINJA_NO_EXCEPTIONS"
+fi
 
 # Makefile is not parallel safe so can't use 'make --jobs="${CPU_COUNT}"'
 make
@@ -30,3 +68,5 @@ if [[ "${CONDA_BUILD_CROSS_COMPILATION:-}" != "1" || "${CROSSCOMPILING_EMULATOR:
 fi
 make install
 make clean
+
+unset DISABLE_QUADMATH
